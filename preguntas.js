83151187b17ee4b9b99c166253f10ed4700fd6d6
ivan <albornozivan03@gmail.com>
2024-10-29 19:02:29 -0300

    first commit

---
+++ preguntas.js
@@ -0,0 +1,84 @@
+const questions = [
+    {
+        id:1,
+        question:"¿p?",
+        correctAnswer: "1",
+        wrongAnswer1: "2",
+        wrongAnswer2: "3",
+        wrongAnswer3: "4"
+    },
+    {
+        id:2,
+        question:"¿?",
+        correctAnswer: "",
+        wrongAnswer1: "",
+        wrongAnswer2: "",
+        wrongAnswer3: ""
+    },
+    {
+        id:3,
+        question:"¿?",
+        correctAnswer: "",
+        wrongAnswer1: "",
+        wrongAnswer2: "",
+        wrongAnswer3: ""
+    },
+    {
+        id:4,
+        question:"¿?",
+        correctAnswer: "",
+        wrongAnswer1: "",
+        wrongAnswer2: "",
+        wrongAnswer3: ""
+    },
+    {
+        id:5,
+        question:"¿?",
+        correctAnswer: "",
+        wrongAnswer1: "",
+        wrongAnswer2: "",
+        wrongAnswer3: ""
+    },
+    {
+        id:6,
+        question:"¿?",
+        correctAnswer: "",
+        wrongAnswer1: "",
+        wrongAnswer2: "",
+        wrongAnswer3: ""
+    },
+    {
+        id:7,
+        question:"¿?",
+        correctAnswer: "",
+        wrongAnswer1: "",
+        wrongAnswer2: "",
+        wrongAnswer3: ""
+    },
+    {
+        id:8,
+        question:"¿?",
+        correctAnswer: "",
+        wrongAnswer1: "",
+        wrongAnswer2: "",
+        wrongAnswer3: ""
+    },
+    {
+        id:9,
+        question:"¿?",
+        correctAnswer: "",
+        wrongAnswer1: "",
+        wrongAnswer2: "",
+        wrongAnswer3: ""
+    },
+    {
+        id:10,
+        question:"¿?",
+        correctAnswer: "",
+        wrongAnswer1: "",
+        wrongAnswer2: "",
+        wrongAnswer3: ""
+    },
+]
+
+export default questions
---
+++ script.js
@@ -0,0 +1,84 @@
+import questions from "./preguntas.js";
+
+const questionDOM = document.querySelector(".question"),
+    resetButton = document.querySelector(".reset"),
+    backButton = document.querySelector(".back"),
+    [option1,option2,option3,option4] = document.querySelectorAll(".options"),
+    options = [option1, option2, option3, option4]
+
+let numberQuestion = 0,
+    answers = [],
+    guessed = 0;
+
+function main() {
+    showQuestion()
+    showAnswers()
+}
+
+function showQuestion() {
+    questionDOM.innerHTML = questions[numberQuestion].question;
+}
+
+function showAnswers() {
+    answers = [questions[numberQuestion].correctAnswer,
+                questions[numberQuestion].wrongAnswer1,
+                questions[numberQuestion].wrongAnswer2,
+                questions[numberQuestion].wrongAnswer3];
+    
+    let randomNumbersList = [];
+    
+    while (randomNumbersList.length < 4) {
+        let randomNumber = Math.floor(Math.random() * 4);
+        if (!randomNumbersList.includes(randomNumber)) {
+            randomNumbersList.push(randomNumber);
+        }
+    }
+    options.forEach((option,index) => option.innerHTML = answers[randomNumbersList[index]])
+}
+
+function checkAnswers(selectedOption) {
+    if (questions[numberQuestion].correctAnswer === selectedOption.textContent) {
+        selectedOption.style.backgroundColor = "green"
+        guessed++
+    } else {
+        selectedOption.style.backgroundColor = "red"
+    options.forEach(option => {
+        if (option.textContent === questions[numberQuestion].correctAnswer) {
+            option.style.backgroundColor = "green";
+        }
+    });
+    }
+
+    setTimeout(() => {
+        changeColors()
+    }, 1200);
+}
+
+function changeColors(){
+    options.forEach(option => option.style.backgroundColor = "");
+    if (numberQuestion < questions.length - 1) {
+        numberQuestion++
+        showQuestion()
+        showAnswers()
+    }
+    else{
+        questionDOM.innerHTML = `Respondiste bien ${guessed} de ${questions.length} preguntas`
+        options.forEach(option => option.style.display = "none")
+    }
+}
+
+function reset(){
+    guessed = 0
+    numberQuestion = 0
+    options.forEach(option => option.style.display = "block")
+    main()
+}
+
+const back = () => window.location.href = "../index.html"
+
+options.forEach(option => option.addEventListener("click", () => checkAnswers(option)))
+
+backButton.addEventListener("click", back)
+resetButton.addEventListener("click", reset)
+
+main()
